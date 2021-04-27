8827a1a57b5bf263013a635da5c4b2040309db6e
Mark Kadiner <dasistmail765@gmail.com>
2021-04-27 21:51:10 +0300

    Node Start

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers)
+    // process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>Node JS</title></head>')
+    res.write('<body>Hello Node JS world :)</body>')
+    res.write('<html/>')
+    res.end()
+})
+
+server.listen(8000)
