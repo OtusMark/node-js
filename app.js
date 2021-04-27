fc3824cf69661a65bb4d8941cf12be797df4f17d
Mark Kadiner <dasistmail765@gmail.com>
2021-04-27 23:00:14 +0300

    Testing

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
-    // process.exit()
+
+    const url = req.url
+    const method = req.method
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>')
+        res.write('<html/>')
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, () => {
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
+        })
+    }
+
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
-    res.write('<head><title>Node JS</title></head>')
-    res.write('<body>Hello Node JS world :)</body>')
+    res.write('<head><title>Main page</title></head>')
+    res.write('<body><h1>Main page</h1></body>')
     res.write('<html/>')
-    res.end()
+    return res.end()
 })
 
 server.listen(8000)
